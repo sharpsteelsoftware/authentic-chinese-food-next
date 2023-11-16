8e6fc0c78403978855255227d74e2a67b201c91f
yuxin8 <zhangyuxin4123671@gmail.com>
2023-11-16 17:30:14 -0600

    add a image

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,18 @@
 import { Debug } from '@/components/debug'
 import { Navigate } from '@/components/nav'
 import { sanityClient } from '@/lib/sanity'
+import Image from 'next/image'
 
 export default async function Home() {
   // const res = await fetch('http://localhost:3333/desk/cuisine');
   // const res = await fetch('https://api.github.com/repos/vercel/next.js');
-  const result = await sanityClient.fetch('*[_type == "cuisine"]')
+  const result = await sanityClient.fetch(
+    '*[_type == "cuisine"]{...,"imageUrl": image.asset->url}',
+  )
   return (
     <main className=''>
-     
-      {/* <Debug data={result} /> */}
+      <Image height={600} width={600} src={result[0].imageUrl} alt='Picture of the author' />
+      <Debug data={result[0]} />
     </main>
   )
 }
